15971f3bb82d16013edb57c3b7a1e877174213a6
MichelLimaDesigner <41237418+MichelLimaDesigner@users.noreply.github.com>
2018-08-17 12:32:41 -0300

    Lading page Cartórios Com.vc

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/*===============================
+    ANIMATION MENU - ANCHOR LINK
+===============================*/
+
+$('document').ready(function() {
+    $('.nav a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            targetOffset = $(id).offset().top;
+
+        $('html, body').animate({
+            scrollTop: targetOffset - 0
+        }, 900);
+    });
+});
